acccea5a789eab3e55557d53ad2587f2dc00e05e
HAUTIER thomas <hautier@informatique.univ-paris-diderot.fr>
2017-06-04 02:29:34 +0200

    Ajout test

---
+++ exemple1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "conduct.h"
+#include <unistd.h>
+
+#include <pthread.h>
+
+
+
+int main(void)
+{
+	char test[5];
+	struct conduct *c;
+	c = conduct_create(NULL, 5,5);
+
+	conduct_write(c, "test\n", 5);
+	
+
+	conduct_read(c, &test, 5);	
+	printf("%s", test);
+	
+	return 0;
+}
